24aec0da6acc4944d516c5009711eebfa3cc8bf6
Brume.Love <brume.love@mns.mu>
2022-10-26 12:45:45 +0400

    code impl to rest container

--- src/test/java/com/test/app/config/ApplicantTestConfig.java
+++ src/test/java/com/test/app/config/ApplicantTestConfig.java
@@ -1,6 +1,7 @@
 package com.test.app.config;
 
 import com.test.app.entity.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @author Brume
  */
 @ComponentScan(basePackages = {"com.test.app"})
-@EnableJpaRepositories(basePackages = "com.test.app)
+@EnableJpaRepositories(basePackages = "com.test.app")
 @EntityScan(basePackageClasses = {RocApplicant.class, RocApplicantsAttach.class, RocApplicantsAdm.class, RocApplicantsUser.class, RocApplicantsContact.class, })
 //@EntityScan(basePackages = "com.test.app.entity")
 public class ApplicantTestConfig {
